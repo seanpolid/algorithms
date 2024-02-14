70e6b69e5083316304b9ec72afb099742ae13a7b
seanpolid <seanpolidori0@gmail.com>
2024-02-13 23:15:13 -0500

    Optimized quick select

--- src/main/java/seanp/algorithms/decrease_and_conquer/QuickSelect.java
+++ src/main/java/seanp/algorithms/decrease_and_conquer/QuickSelect.java
@@ -1,30 +1,101 @@
 package seanp.algorithms.decrease_and_conquer;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class QuickSelect {
 
 	private static Random random = new Random(); 
 
-    public int findKthLargest(int[] nums, int k) {
+    public static int findKthLargest(int[] nums, int k) {
         return quickSelect(0, nums.length - 1, nums, k);
     }
 
-    private int quickSelect(int low, int high, int[] nums, int k) {
-        int s = partition(low, high, nums, k);
+    private static int quickSelect(int low, int high, int[] nums, int k) {
+        int s = partitionRandomized(low, high, nums, k);
 
         int pos = high - s + 1;
         if (pos == k) 
             return nums[s];
         else if (pos < k) 
-            return quickSelect(low, s - 1, nums, k - s);
+            return quickSelect(low, s - 1, nums, k - pos);
         else 
             return quickSelect(s + 1, high, nums, k);
     }
+    
+    private static int partitionMOM(int low, int high, int[] nums, int k) {
+    	int pivot = findMedianOfMedians(low, high, nums);
+    	int pivotIndex = findIndex(low, nums, pivot);
+    	
+        swap(nums, low, pivotIndex);
+        pivotIndex = low;
+        
+        int s = low;
+        for (int i = low; i <= high; i++) {
+            if (nums[i] < nums[pivotIndex]) {
+                swap(nums, ++s, i);
+            }
+        }
 
-    private int partition(int low, int high, int[] nums, int k) {
-        int pivotIndex = low;
+        swap(nums, low, s);
+        
+        return s;
+    }
+    
+    private static int findMedianOfMedians(int low, int high, int[] nums) {
+    	int numMedians = (high - low) / 5 + 1;
+    	int[] medians = new int[numMedians];
+    	
+    	for (int i = 0; i < numMedians; i++) {
+    		int start = low + (5 * i);
+    		int end = start + Math.min(5, high - start + 1);
+            
+    		int[] array = Arrays.copyOfRange(nums, start, end);
+    		Arrays.sort(array);
+    		
+    		int mid = array.length / 2;
+    		medians[i] = array[mid];
+    	}
+    	
+    	if (medians.length == 1) {
+    		return medians[0];
+    	} else {
+    		return findMedianOfMedians(0, medians.length - 1, medians);
+    	}
+    }
 
+    private static int findIndex(int low, int[] nums, int target) {
+        int index = low;
+        for (int i = low; i < nums.length; i++) {
+            if (nums[i] == target) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    private static int partitionRandomized(int low, int high, int[] nums, int k) {
+        int pivotIndex = low == high ? low : low + random.nextInt(high - low);
+        swap(nums, low, pivotIndex);
+        pivotIndex = low;
+        
+        int s = low;
+        for (int i = low; i <= high; i++) {
+            if (nums[i] < nums[pivotIndex]) {
+                swap(nums, ++s, i);
+            }
+        }
+
+        swap(nums, low, s);
+        
+        return s;
+    }
+    
+    private static int partitionBasic(int low, int high, int[] nums, int k) {
+    	int pivotIndex = low;
+        
         int s = low;
         for (int i = low; i <= high; i++) {
             if (nums[i] < nums[pivotIndex]) {
@@ -37,7 +108,7 @@ public class QuickSelect {
         return s;
     }
 
-    private void swap(int[] nums, int i, int j) {
+    private static void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
--- src/test/java/seanp/algorithms/decrease_and_conquer/QuickSelectTests.java
+++ src/test/java/seanp/algorithms/decrease_and_conquer/QuickSelectTests.java
@@ -1,12 +1,76 @@
 package seanp.algorithms.decrease_and_conquer;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class QuickSelectTests {
 
 	@Test
 	public void quickSelect_provided() {
+		// Arrange
+		int[] nums = {3, 2, 1, 5, 6, 4};
+		int expected = 5;
 		
+		// Act
+		int actual = QuickSelect.findKthLargest(nums, 2);
+		
+		// Assert
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void quickSelect_provided2() {
+		// Arrange
+		int[] nums = {3,2,3,1,2,4,5,5,6};
+		int expected = 4;
+		
+		// Act
+		int actual = QuickSelect.findKthLargest(nums, 4);
+		
+		// Assert
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void quickSelect_decreasing() {
+		// Arrange
+		int[] nums = {6, 5, 4, 3, 2, 1};
+		int expected = 3;
+		
+		// Act
+		int actual = QuickSelect.findKthLargest(nums, 4);
+		
+		// Assert
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void quickSelect_manyRepeats() {
+		// Arrange
+		int[] nums = createManyRepeats(5000);
+		int expected = -5;
+		
+		// Act
+		int actual = QuickSelect.findKthLargest(nums, 5000);
+		
+		// Assert
+		assertEquals(expected, actual);
+	}
+	
+	private int[] createManyRepeats(int n) {
+		int[] nums = new int[n];
+		for (int i = 0; i < 5; i++) {
+			nums[i] = i;
+		}
+		for (int i = 5; i < n - 5; i++) {
+			nums[i] = 1;
+		}
+		
+		for (int i = n - 5; i < n; i++) {
+			nums[i] = - (i - n + 6);
+		}
+		
+		return nums;
 	}
 	
 }
